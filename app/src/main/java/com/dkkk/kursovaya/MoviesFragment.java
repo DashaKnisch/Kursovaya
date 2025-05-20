7633f5167e91158165a80a6a3cce3ef8af324e65
DashaKnisch <maria.kurchanova02@gmail.com>
2025-05-21 01:03:03 +0300

    add something

--- app/src/main/java/com/dkkk/kursovaya/MoviesFragment.java
+++ app/src/main/java/com/dkkk/kursovaya/MoviesFragment.java
@@ -4,18 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MoviesFragment extends Fragment {
-    public MoviesFragment() {}
+
+    private WebView webView;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_movies, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_movies, container, false);
+        webView = view.findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://www.ivi.ru/movies");
+
+        return view;
     }
 }
